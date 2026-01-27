c71f0274c8e2faecba1902050b6e67c3288bab57
ra7eem. <kassim.lemke32@gmail.com>
2026-01-27 18:19:47 +0100

    changed nvim UI Logo

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -21,6 +21,67 @@ return {
   {
     "nvimdev/dashboard-nvim",
     enabled = true,
+    opts = function()
+      local logo = [[
+  _   _  ______ __      __ ______  _____       _____  _____ __      __ ______     _    _  _____  
+ | \ | ||  ____|\ \    / /|  ____||  __ \     / ____||_   _|\ \    / /|  ____|   | |  | ||  __ \ 
+ |  \| || |__    \ \  / / | |__   | |__) |   | |  __   | |   \ \  / / | |__      | |  | || |__) |
+ | . ` ||  __|    \ \/ /  |  __|  |  _  /    | | |_ |  | |    \ \/ /  |  __|     | |  | ||  ___/ 
+ | |\  || |____    \  /   | |____ | | \ \    | |__| | _| |_    \  /   | |____    | |__| || |     
+ |_| \_||______|    \/    |______||_|  \_\    \_____||_____|    \/    |______|    \____/ |_|     
+  ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+      -- stylua: ignore
+      center = {
+        { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+        { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+        { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+        { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+        { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+        { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+        { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+        { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+      },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- open dashboard after closing lazy
+      if vim.o.filetype == "lazy" then
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+            end)
+          end,
+        })
+      end
+
+      return opts
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
@@ -73,11 +134,12 @@ return {
 
   {
     "rcarriga/nvim-notify",
-    opts = {
-      timeout = 5000,
-      background_colour = "#000000",
-      render = "wrapped-compact",
-    },
+    enabled = false,
+    -- opts = {
+    --   timeout = 5000,
+    --   background_colour = "#000000",
+    --   render = "wrapped-compact",
+    -- },
   },
 
   -- buffer line
@@ -105,10 +167,10 @@ return {
         offsets = {
           {
             filetype = "neo-tree",
-            text = "File Explorer",
+            text = "Neo-tree",
             highlight = "Directory",
             text_align = "left",
-            seperator = true,
+            sepator = true,
           },
           {
             filetype = "snacks_layout_box",
